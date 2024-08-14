1abf2382260ade84909122da41046e6fd2135c47
DmitryZherebtsov <79690647+DmitryZherebtsov@users.noreply.github.com>
2024-08-14 14:39:59 +0200

    2Update index.js

--- index.js
+++ index.js
@@ -1,133 +1,138 @@
+const { Telegraf } = require('telegraf');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { Telegraf } = require('telegraf');
 
 const { sendOptionsKeyboard } = require('./Keyboard');
-const { handleFeedbacks, handleAddReview, handleRejectReview, addReviews, confirmedReviews } = require('./Feedbacks');
-const { handleDeleteFromReviews, handleFeedbackElements } = require('./DeleteFeedback');
-const { addOrder, handleOrders } = require('./Orders');
+const { sendReview, reviewIndexMap } = require('./Feedbacks');
+const { handleFeedbackElements, handleDeleteFromReviews } = require('./DeleteFeedback');
+const { sendOrder } = require('./Orders');
 
-const token = '7429375772:AAHwF97qO1tD0NveFrO-pc-nKDLWxJtoClA';
-const bot = new Telegraf(token);
+const bot = new Telegraf('7229021767:AAF-0lwuhfCXzlC_uDOrgxX1a0LCwCTjgYo');
 
 const app = express();
 const port = 3001;
-const allowedUsers = [786187640, 1027031193];
 
 app.use(cors());
 app.use(bodyParser.json());
 
+const allowedUsers = [786187640, 1027031193];
+const allowedUsersForReviews = [786187640, 1027031193];
+
+let confirmedReviews = [
+  { firstName: 'Іван', lastName: 'Іванов', comment: 'Дуже сподобалося!', phone: '123-456-7890', rating: 5 },
+  { firstName: 'Марія', lastName: 'Петренко', comment: 'Все було добре.', phone: '098-765-4321', rating: 4 },
+  { firstName: 'Олександр', lastName: 'Сидоренко', comment: 'Може бути краще.', phone: '234-567-8901', rating: 3 }
+];
+
+//---------------------------checkUserPermission---------------------------//
 const checkUserPermission = (ctx, next) => {
-    const userId = ctx.from.id;
-    if (allowedUsers.includes(userId)) {
-        return next();
-    } else {
-        ctx.reply('У вас немає доступу до цього бота.');
-        console.log(`${userId} непройшов перевірку!`);
-    }
+  const userId = ctx.from.id;
+  if (allowedUsers.includes(userId)) {
+      return next();
+  } else {
+      ctx.reply('У вас немає доступу до цього бота.');
+      console.log(`${userId} не пройшов перевірку!`);
+  }
 };
 
-function sendNotification(newMessage) {
-    const message = newMessage;
-    allowedUsers.forEach(chatId => {
-        bot.telegram.sendMessage(chatId, message)
-            .then(response => {
-                console.log(`Повідомлення відправлено до chatId: ${chatId}`, response);
-            })
-            .catch(error => {
-                console.error(`Помилка надсилання повідомлення до chatId: ${chatId}`, error);
-            });
-    });
-}
+const checkUserPermissionForReviews = (ctx, next) => {
+  const userId = ctx.from.id;
+  if (allowedUsersForReviews.includes(userId)) {
+      return true;
+  } else {
+      ctx.reply('У вас немає доступу відгуків.');
+      console.log(`${userId} не пройшов перевірку!`);
+      return false;
+  }
+};
 
-app.post('/api/reviews', (req, res) => {
-    const newReviews = req.body;
-
-    if (typeof newReviews !== 'object' || newReviews === null) {
-        console.log("Неправильний формат даних. Очікувався об'єкт.");
-        return res.status(400).send("Неправильний формат даних. Очікувався об'єкт.");
-    }
-    try {
-        sendNotification('У вас новий відгук!');
-        addReviews(newReviews);
-        res.status(200).send('Відгуки успішно оновлено');
-        console.log('Відгуки успішно оновлено');
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
+//---------------------------Get Orders---------------------------//
 app.post('/api/data', async (req, res) => {
-    const newOrder = req.body;
-
-    try {
-        sendNotification('У вас нове замовлення!');
-        console.log(newOrder);
-        addOrder(newOrder);
-        res.status(200).send('Замовлення успішно оновлено');
-        console.log('Замовлення успішно оновлено');
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  const newOrder = req.body;
+  try {
+    sendOrder(newOrder, allowedUsers, bot);
+  } catch (error) {
+      res.status(500).send(error.message);
+  }
 });
 
+//---------------------------Get Reviews---------------------------//
+app.post('/api/reviews', (req, res) => {
+  const newReviews = req.body;
+  if (typeof newReviews !== 'object' || newReviews === null) {
+      return res.status(400).send("Неправильний формат даних. Очікувався об'єкт.");
+  }
+  try {
+    sendReview(newReviews, allowedUsersForReviews, bot);
+  } catch (error) {
+      res.status(500).send(error.message);
+  }
+});
+
+//---------------------------Send Reviews---------------------------//
 app.get('/api/getReviews', (req, res) => {
-    res.json(confirmedReviews);
-    console.log("Підтверджені відгуки надіслано!");
+  res.json(confirmedReviews);
+  console.log("Підтверджені відгуки надіслано на сайт!");
 });
 
-app.listen(port, () => {
-    console.log(`Сервер працює на http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('Express is working correctly 0.0');
 });
+
 
 bot.use(checkUserPermission);
 
 bot.start((ctx) => {
-    console.log(`Отримано повідомлення від chat ID: ${ctx.chat.id}`);
-    sendOptionsKeyboard(ctx);
+  console.log(`Отримано повідомлення від chat ID: ${ctx.chat.id}`);
+  sendOptionsKeyboard(ctx);
 });
 
-//--------------------------------Feedbacks--------------------------------//
-
-bot.hears('Відгуки', (ctx) => {
-    handleFeedbacks(ctx);
-});
-
-//--------------------------------Orders--------------------------------//
-
-bot.hears('Замовлення', (ctx) => {
-    handleOrders(ctx, allowedUsers);
-    sendOptionsKeyboard(ctx);
-});
-
-//--------------------------------Delete from Feedbacks--------------------------------//
 
 bot.hears('Видалити відгук', (ctx) => {
-    handleFeedbackElements(ctx);
+  if(checkUserPermissionForReviews(ctx))
+    handleFeedbackElements(ctx, confirmedReviews);
 });
 
-bot.action(/add_(.+)/, (ctx) => {
-    const reviewIndex = parseInt(ctx.match[1]);
-    handleAddReview(ctx, reviewIndex);
+bot.action(/add_(\d+)/, (ctx) => {
+  const index = parseInt(ctx.match[1]);
+  const review = reviewIndexMap.get(index);
+
+  if (review) {
+      confirmedReviews.push(review);
+      ctx.reply('Відгук додано!');
+      ctx.deleteMessage();
+      reviewIndexMap.delete(index);
+  } else {
+      ctx.reply('Відгук не знайдено.');
+  }
 });
 
-bot.action(/reject_(.+)/, (ctx) => {
-    handleRejectReview(ctx);
+bot.action(/reject_(\d+)/, (ctx) => {
+  const index = parseInt(ctx.match[1]);
+  const review = reviewIndexMap.get(index);
+
+  if (review) {
+      ctx.reply('Відгук видалено!');
+      ctx.deleteMessage();
+      reviewIndexMap.delete(index);
+  } else {
+      ctx.reply('Відгук не знайдено.');
+  }
 });
 
 bot.action(/deleteReview_(.+)/, (ctx) => {
-    handleDeleteFromReviews(ctx);
+  handleDeleteFromReviews(ctx, confirmedReviews);
 });
 
-//--------------------------------Debuging--------------------------------//
-
 bot.catch((err, ctx) => {
-    console.error(`Помилка!!! ${ctx.updateType}`, err);
+  console.error(`Помилка!!!!!!! ${ctx.updateType}`, err);
+});
+
+app.listen(port, () => {
+  console.log(`Сервер працює: http://localhost:${port}`);
 });
 
 bot.launch();
 
-module.exports = {
-    sendOptionsKeyboard
-}
+
