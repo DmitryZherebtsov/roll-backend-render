49cf151444fd25f74fd99291a11d31ddc2cb5887
DmitryZherebtsov <79690647+DmitryZherebtsov@users.noreply.github.com>
2024-08-14 14:39:13 +0200

    2Update DeleteFeedback.js

--- DeleteFeedback.js
+++ DeleteFeedback.js
@@ -1,23 +1,22 @@
 const { Markup } = require('telegraf');
 const { sendOptionsKeyboard } = require('./Keyboard');
-const { confirmedReviews } = require('./Feedbacks');
 let message_ids =[]
 
-function handleFeedbackElements(ctx) {
+function handleFeedbackElements(ctx, confirmedReviews) {
     if (confirmedReviews.length === 0){
         ctx.reply("На сайті нема відгуків!")
         sendOptionsKeyboard(ctx);
     }  
     else{
         confirmedReviews.forEach((_, index) => {
-            sendReviewElements(ctx, index);
+            sendReviewElements(ctx, index, confirmedReviews);
         });
         sendOptionsKeyboard(ctx);
     }
     
 }
 
-async function sendReviewElements(ctx, index) {
+async function sendReviewElements(ctx, index, confirmedReviews) {
     const review = confirmedReviews[index];
     const message = await ctx.reply(`Ім'я: ${review.firstName}\nПрізвище: ${review.lastName}\nТелефон: ${review.phone}\nВідгук: ${review.comment}\nОцінка: ${review.rating}`, 
         Markup.inlineKeyboard([
@@ -27,7 +26,7 @@ async function sendReviewElements(ctx, index) {
     message_ids.push(message.message_id);
 }
 
-function handleDeleteFromReviews(ctx){
+function handleDeleteFromReviews(ctx, confirmedReviews){
     const reviewIndex = parseInt(ctx.match[1]);
     ctx.reply('Елемент успішно видалено');
     message_ids.forEach(message_id => {
@@ -35,13 +34,13 @@ function handleDeleteFromReviews(ctx){
             .catch(err => console.error(`Не вдалося видалити повідомлення з id ${message_id}:`, err));
     });
     confirmedReviews.splice(reviewIndex, 1);
-    message_ids = []
     console.log(confirmedReviews);
+    message_ids = []
+
     sendOptionsKeyboard(ctx);
 }
 
 module.exports = {
     handleFeedbackElements,
-    handleDeleteFromReviews,
-    sendReviewElements
+    handleDeleteFromReviews
 };
